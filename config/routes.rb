7705b76e531503c3c5cc817b991c7510f99a43b2
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-04-16 15:22:30 +0000

    [Progress_6]ユーザー機能のコントローラの作成、productionカラムの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  resources :users
   resources :books
+  
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
