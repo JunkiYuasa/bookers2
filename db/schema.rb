4bafec60f8bd4e70c80eba839939be0cf0ed3f45
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-04-16 07:45:16 +0000

    [progrss_2]Bookモデル、Bookコントローラーの作成、newアクションの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_16_034712) do
+ActiveRecord::Schema.define(version: 2024_04_16_070213) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "book_name"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
